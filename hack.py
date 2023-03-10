2b65329cc85d9117603e97bf8464bbe8ca5dd5f8
XX-Crafter-XX <91060445+XX-Crafter-XX@users.noreply.github.com>
2023-03-10 18:36:26 +0100

    Update hack.py

--- hack.py
+++ hack.py
@@ -4,9 +4,9 @@ import multiprocessing
 possible_digits = '0123456789'
 password_length = 10
 
-# Define a function to generate passwords
-def generate_passwords(start, end):
-    with open('passwords.txt', 'a') as file:
+# Define a function to generate passwords and write them to a file
+def generate_passwords(start, end, filename):
+    with open(filename, 'a') as file:
         for password in itertools.product(possible_digits, repeat=password_length):
             password_str = ''.join(password)
             file.write(password_str + '\n')
@@ -17,12 +17,12 @@ if __name__ == '__main__':
 
     # Split the password generation task into equal chunks for each core
     chunk_size = len(possible_digits) ** password_length // num_cores
-    chunks = [(i * chunk_size, (i+1) * chunk_size) for i in range(num_cores)]
+    chunks = [(i * chunk_size, (i+1) * chunk_size, 'passwords.txt') for i in range(num_cores)]
 
     # Start a separate process for each core
     processes = []
-    for start, end in chunks:
-        p = multiprocessing.Process(target=generate_passwords, args=(start, end))
+    for start, end, filename in chunks:
+        p = multiprocessing.Process(target=generate_passwords, args=(start, end, filename))
         processes.append(p)
         p.start()
 
